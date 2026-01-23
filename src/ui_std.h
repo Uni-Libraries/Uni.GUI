a7a5469e15b0979a8e7bbafb1eddde630edf6a3b
Mikhail Paulyshka <me@mixaill.net>
2026-01-23 15:23:11 +0300

    inline ui_std

--- src/ui_std.h
+++ src/ui_std.h
@@ -18,36 +18,66 @@
 //
 
 namespace ImGui {
-    bool Begin(const std::string &name, bool *p_open = nullptr, ImGuiWindowFlags flags = 0);
+    inline bool Begin(const std::string &name, bool *p_open = nullptr, ImGuiWindowFlags flags = 0) {
+        return ImGui::Begin(name.c_str(), p_open, flags);
+    }
 
-    bool BeginPopupModal(const std::string& name, bool* p_open = nullptr, ImGuiWindowFlags flags = 0);
+    inline bool BeginPopupModal(const std::string& name, bool* p_open = nullptr, ImGuiWindowFlags flags = 0) {
+        return ImGui::BeginPopupModal(name.c_str(), p_open, flags);
+    }
 
-    bool BeginTabItem(const std::string& label, bool* p_open = nullptr, ImGuiTabItemFlags flags = 0);
+    inline bool BeginTabItem(const std::string& label, bool* p_open = nullptr, ImGuiTabItemFlags flags = 0) {
+        return ImGui::BeginTabItem(label.c_str(), p_open, flags);
+    }
 
-    void BulletText(const std::string &text);
+    inline void BulletText(const std::string &text){
+        ImGui::BulletText("%s", text.c_str());
+    }
 
-    void BulletText(std::string_view text);
+    inline void BulletText(std::string_view text){
+        ImGui::BulletText("%s", text.data());
+    }
 
-    bool Button(const std::string &label, const ImVec2 &size_arg = ImVec2(0, 0));
+    inline bool Button(const std::string &label, const ImVec2 &size_arg = ImVec2(0, 0)){
+        return ImGui::Button(label.c_str(), size_arg);
+    }
 
-    bool Checkbox(const std::string& label, bool* v);
+    inline bool Checkbox(const std::string& label, bool* v) {
+        return ImGui::Checkbox(label.c_str(), v);
+    }
 
-    bool CollapsingHeader(const std::string& label, ImGuiTreeNodeFlags flags = 0);
+    inline bool CollapsingHeader(const std::string& label, ImGuiTreeNodeFlags flags = 0) {
+        return ImGui::CollapsingHeader(label.c_str(), flags);
+    }
 
-    void OpenPopup(const std::string& id, ImGuiPopupFlags popup_flags = 0);
+    inline void OpenPopup(const std::string& id, ImGuiPopupFlags popup_flags = 0){
+        ImGui::OpenPopup(id.c_str(), popup_flags);
+    }
 
-    bool RadioButton(const std::string& label, bool active);
+    inline bool RadioButton(const std::string& label, bool active) {
+        return ImGui::RadioButton(label.c_str(), active);
+    }
 
-    void TableSetupColumn(const std::string &label, ImGuiTableColumnFlags flags = 0, float init_width_or_weight = 0.0f,
-                          ImGuiID user_id = 0);
+    inline void TableSetupColumn(const std::string &label, ImGuiTableColumnFlags flags = 0, float init_width_or_weight = 0.0f,
+                          ImGuiID user_id = 0) {
+        ImGui::TableSetupColumn(label.c_str(), flags, init_width_or_weight, user_id);
+    }
 
-    void TextColored(const ImVec4& col, const std::string& fmt);
+    inline void TextColored(const ImVec4& col, const std::string& fmt) {
+        ImGui::TextColored(col, "%s", fmt.c_str());
+    }
 
-    void TextUnformatted(const std::string &text);
+    inline void TextUnformatted(const std::string &text) {
+        ImGui::TextUnformatted(text.c_str());
+    }
 
-    bool TreeNode(const std::string& label);
+    inline bool TreeNode(const std::string& label) {
+        return ImGui::TreeNode(label.c_str());
+    }
 }
 
 namespace ImPlot {
-    bool BeginPlot(const std::string& title_id, const ImVec2& size, ImPlotFlags flags);
+    inline bool BeginPlot(const std::string& title_id, const ImVec2& size, ImPlotFlags flags) {
+        return ImPlot::BeginPlot(title_id.c_str(), size, flags);
+    }
 }
